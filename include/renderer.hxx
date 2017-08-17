dae0217d25fe3d32373c78bff0416e6904d99e61
nshcat <nshcat@users.noreply.github.com>
2017-08-17 21:12:20 +0200

    Implemented render manager

---
+++ include/renderer.hxx
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "screen.hxx"
+#include "uniform.hxx"
+#include "program.hxx"
+#include "texture.hxx"
+#include "lighting.hxx"
+#include "render_context.hxx"
+#include "empty_vbo.hxx"
+
+class render_manager
+{
+	using dimension_type = glm::uvec2;
+	using handle_type = render_context::handle_type;
+	using texture_type = const texture_manager&;
+	using context_type = render_context&;
+
+	public:
+		render_manager(	render_context& p_context,
+						const dimension_type& p_screenDims,
+						const texture_manager& p_texMgr );
+		
+	public:		
+		auto screen()
+			-> screen_manager&;
+			
+		auto light_manager()
+			-> light_manager&;
+			
+		auto render()
+			-> void;
+			
+		auto begin_frame()
+			-> void;
+			
+		auto end_frame()
+			-> void;
+	
+	private:
+		auto set_uniforms()
+			-> void;
+					
+	private:
+		context_type m_Context;
+		texture_type m_Tex;
+		gl::program m_Program;
+		class light_manager m_Lighting;
+		screen_manager m_Screen;
+		empty_vbo m_Vbo;
+		dimension_type m_GlyphCount;
+};
+
---
+++ src/render_manager.cxx
@@ -0,0 +1,85 @@
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/transform.hpp>
+#include <renderer.hxx>
+
+render_manager::render_manager(	render_context& p_context,
+								const dimension_type& p_screenDims,
+								const texture_manager& p_texMgr )
+	: 	m_Tex{p_texMgr},
+		m_Screen{p_screenDims},
+		m_Lighting{ },
+		m_Vbo{ },
+		m_Context{p_context},
+		m_GlyphCount{p_screenDims},
+		m_Program{
+			gl::vertex_shader{ gl::from_file, "assets/ascii.vs.glsl" },
+			gl::fragment_shader{ gl::from_file, "assets/ascii.fs.glsl" },
+		}	
+{
+	p_context.resize({p_texMgr.glyph_size().x * p_screenDims.x, p_texMgr.glyph_size().y * p_screenDims.y});
+	m_Program.use();
+	set_uniforms();
+}
+
+
+auto render_manager::screen()
+	-> screen_manager&
+{
+	return m_Screen;
+}
+
+auto render_manager::set_uniforms()
+	-> void
+{
+	// Set uniforms		
+	gl::set_uniform(m_Program, "fog_color", glm::vec4{ 0.1f, 0.1f, 0.3f, 1.f });
+	gl::set_uniform(m_Program, "fog_density", 5.f);
+	gl::set_uniform(m_Program, "projection_mat", glm::ortho(0.f, static_cast<float>(m_Context.dimensions().x), static_cast<float>(m_Context.dimensions().y), 0.f, -1.f, 1.f));		
+	gl::set_uniform(m_Program, "sheet_dimensions", glm::ivec2{ texture_manager::sheet_width, texture_manager::sheet_height });
+	gl::set_uniform(m_Program, "glyph_dimensions", m_Tex.glyph_size());
+	gl::set_uniform(m_Program, "glyph_count", glm::ivec2{m_GlyphCount});
+	
+	// Samplers are just integers
+	gl::set_uniform(m_Program, "text_texture", 0);
+	gl::set_uniform(m_Program, "graphics_texture", 1);	
+	gl::set_uniform(m_Program, "shadow_texture", 2);
+	gl::set_uniform(m_Program, "input_buffer", 3);
+}
+
+auto render_manager::render()
+	-> void
+{
+	// Sync state with gpu
+	m_Lighting.sync();
+	m_Screen.sync();
+	
+	// Reset openGl state
+	m_Program.use();
+	m_Vbo.use();
+	m_Tex.use();
+	
+	// Render
+	glDrawArraysInstanced(GL_TRIANGLES, 0, 6, m_GlyphCount.x * m_GlyphCount.y);
+}
+
+auto render_manager::end_frame()
+	-> void
+{
+	// TODO maybe this should be done in context.
+	glfwSwapBuffers(m_Context.handle());
+}
+
+auto render_manager::begin_frame()
+	-> void
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+auto render_manager::light_manager()
+	-> class light_manager&
+{
+	return m_Lighting;
+}
