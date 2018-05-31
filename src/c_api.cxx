fca4d268bbf269e04259d116d02730a39e196437
nshcat <nshcat@users.noreply.github.com>
2018-05-31 14:57:42 +0200

    Added C API to path manager

--- src/c_api.cxx
+++ src/c_api.cxx
@@ -207,6 +207,26 @@ extern "C"
 				
 		}
 	}
+	
+	const char* path_get_data_path()
+	{
+		const auto p = global_state<path_manager>().data_path().string();
+		char* str = new char[p.size()+1];
+		::std::copy(p.begin(), p.end(), str);
+		str[p.size()] = '\0';
+		
+		return str;
+	}
+	
+	const char* path_get_user_path()
+	{
+		const auto p = global_state<path_manager>().user_path().string();
+		char* str = new char[p.size()+1];
+		::std::copy(p.begin(), p.end(), str);
+		str[p.size()] = '\0';
+		
+		return str;
+	}
 }
 
 
