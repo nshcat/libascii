439dc5288ffd570265c7c620001dd403eacdcb81
nshcat <nshcat@users.github.com>
2018-08-12 20:20:27 +0200

    Fixed build_settings not compiling on Windows

--- src/build_settings.cxx
+++ src/build_settings.cxx
@@ -1,9 +1,14 @@
+#include <ut/platform.hxx>
 #include <build_settings.hxx>
 
 auto build_settings::data_path_prefix()
 	-> path_type
 {
+#ifdef LIBUT_IS_WINDOWS
+	return { "ROGUELIKE_DATA_PATH" };
+#else
 	return { ROGUELIKE_DATA_PATH };
+#endif
 }
 
 auto build_settings::use_home()
